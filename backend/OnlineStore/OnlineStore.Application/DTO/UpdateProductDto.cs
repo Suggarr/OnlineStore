4208ca31c929fc9f73d76cc2abe164a969f5a732
Ilya Saharevich <247saharevich@gmail.com>
2025-06-24 22:34:59 +0300

    Added some changes to the client's user interface and tested it

--- backend/OnlineStore/OnlineStore.Application/DTO/UpdateProductDto.cs
+++ backend/OnlineStore/OnlineStore.Application/DTO/UpdateProductDto.cs
@@ -5,11 +5,11 @@ namespace OnlineStore.Application.DTO
     public class UpdateProductDto
     {
         [Required]
-        [MaxLength(100)]
+        [MaxLength(50)]//было 100
         public string Name { get; set; } = default!;
 
         [Required]
-        [MaxLength(500)]
+        [MaxLength(200)]//было 500
         public string Description { get; set; } = default!;
 
         [Range(0.01, 100000)]
